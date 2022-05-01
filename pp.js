5e40815076296a34e9631f13e3db3db4e56bfe0f
Davido <davzer@hotmail.com>
2022-05-01 01:43:39 -0500

    pruebita

---
+++ pp.js
@@ -0,0 +1,10 @@
+const array = [
+    { id: "hola", pass: "abc"},
+    { id: "pepon", pass: "abc"},
+    { id: "chau", pass: "xyz"}
+]
+
+
+const valor = array.find(element => true)
+console.log(valor)
+
